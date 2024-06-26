a64ecfe64fc2979edbeddda2535547430b24cf64
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-06-26 12:18:56 +0530

    University:Ch:1-(3)

---
+++ three.py
@@ -0,0 +1,17 @@
+#(3)=>Write a program to create a byte type array, read, modify and display the elements the array.
+
+number=6
+
+arr=bytes(number)
+
+print(arr)
+
+arr=bytes(3)
+
+print(arr)
+
+list=[1,2,3,4]
+
+arr=bytes(list)
+
+print(arr)
