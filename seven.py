beb0ffcf3387b79abe485baa9342b44cfc2af83d
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-06-27 19:35:46 +0530

    University:Ch:1-(7)

---
+++ seven.py
@@ -0,0 +1,13 @@
+#(7)=>Write a program that evaluates an expression given by the user at run time using eval() function.
+
+'''
+
+Example:
+
+Enter any Expression => 10+8-9*2-(10*2)
+Result=> -20
+'''
+
+x=input("Enter any Expression=>")
+
+print("Result=>",eval(x))
