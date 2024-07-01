5fc5ef57c7ce490a4747cee2472ce5f0cf6d139f
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-01 20:28:18 +0530

    University:Ch:2-(5)

---
+++ 25.py
@@ -0,0 +1,9 @@
+#(5)=> Create a program to search the position of an element in an array using index() method of array class.
+
+list1=['hello','A','B','C']
+
+print(list1)
+
+position=list1.index(input("Enter Element from above list:"))
+
+print(position)
