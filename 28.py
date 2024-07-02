ea6858d187f3d89262dcf465e58138e8881acc31
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-02 20:52:08 +0530

    University:Ch:2-(8)

---
+++ 28.py
@@ -0,0 +1,11 @@
+#(28)=> Write a program to pass a list to a function and display it.
+
+def display_list(list1):
+
+    for i in list1:
+
+        print(i)
+
+list1=[1,2,3,4,5,6,7,8,9]
+
+display_list(list1)
