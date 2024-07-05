9311de8f29c76fcc4669ccffcc4d29331530e194
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-05 20:57:57 +0530

    University:Ch:2-(14)

---
+++ 34.py
@@ -0,0 +1,22 @@
+#(14)=> Write a program to create a list using range functions and perfrom append, update and delete elements operations in it.
+
+list1=[i for i in range(0,9)]
+
+print(list1)
+
+list1.append(12)
+
+print(list1)
+
+
+#update
+
+list1.insert(1,15)
+
+print(list1)
+
+#delete
+
+list1.remove(7)
+
+print(list1)
