d8e5e264226f9b7baf0aeca0dd2e41598545cc8b
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-06 19:34:40 +0530

    University:Ch:2-(18)

---
+++ 38.py
@@ -0,0 +1,11 @@
+#(18)=> Write a programm to accpect elements in the form of a tuple and display its minimum,maximum,sum & average.
+
+tup=(1,2,3,4,5,6,7,8)
+
+print("Max=>",max(tup))
+
+print("Min=>",min(tup))
+
+print("Sum=>",sum(tup))
+
+print("Avg=>",sum(tup)/len(tup))
