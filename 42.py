4663d168b45d9089615d9254da34d5f191bbd19c
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-07 18:13:52 +0530

    University:Ch:2-(22)

---
+++ 42.py
@@ -0,0 +1,10 @@
+#(42)=> Write a program to convert the elements of two lists into key value pairs of a dictionary.
+
+keys=['Cricket','BasketBall','Hockey']
+
+values=['11','5','7']
+
+combine_dictionary=dict(zip(keys,values))
+
+print(combine_dictionary)
+
