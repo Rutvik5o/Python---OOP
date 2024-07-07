d53134a83cf48fcd4b221b0cb8b897776545671f
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-07 18:13:06 +0530

    University:Ch:2-(20)

---
+++ 40.py
@@ -0,0 +1,9 @@
+#(20)=>Write a program to create a dictionery from the user and display the elements
+
+my_dict= {'name':"xyz",'age':17,'address':"pata nahi"}
+
+print(my_dict['name'])
+
+print(my_dict['age'])
+
+print(my_dict['address'])
