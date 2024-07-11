7620499205693e2a1e988fc5efd4a19f2e255981
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-11 20:36:19 +0530

    University:Ch:3-(9)

---
+++ 59(two).py
@@ -0,0 +1,31 @@
+# newstudent.py
+
+from student59 import student
+
+
+s=student()
+
+s.setid(100)
+
+s.setname("Xyz")
+
+s.setaddress("Muje nahi pata")
+
+s.setmarks(97)
+
+
+print("Student ID=> ",s.getid())
+
+print("Student Name=> ",s.getname())
+
+print("Student Address=> ",s.getaddress())
+
+print("Student Marks=> ",s.getmarks())
+
+
+
+
+
+
+
+
---
+++ student59.py
@@ -0,0 +1,46 @@
+#(9)=> create a student class to with the methods set_id, get_id, set_name,get_name, set_marks and get_marks whre the method name starting with st are used to assign the values and method
+#name starting with get are returning, the values. Save the program by student.py . Create another program to use th student class which is already available in student.py
+
+
+#Student.py
+
+class student:
+
+    def setid(self,id):
+
+        self.id=id
+
+    def getid(self):
+
+        return self.id
+    
+    def setname(self,name):
+        
+        self.name=name
+
+    def getname(self):
+
+        return self.name
+    
+    def setaddress(self,address):
+
+        self.address=address
+
+    def getaddress(self):
+
+        return self.address
+    
+    def setmarks(self,marks):
+
+        self.marks=marks
+
+    def getmarks(self):
+
+        return self.marks
+    
+    
+
+    
+
+
+
