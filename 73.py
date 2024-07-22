f691b3f1a02f92c7efb2024c6732eaf0cb7b5dc3
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-22 21:31:07 +0530

    University:Ch:4-(3)

---
+++ 73.py
@@ -0,0 +1,10 @@
+#(3)=> Write a program to import "os" module and to print the current working directory and returns a list of all module functions.
+
+
+import os
+
+
+print(os.getcwd())
+
+
+print(dir(os))
