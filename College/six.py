8aa012fe19ab4e73b9c1e7d54747de9d73a142ee
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-28 12:52:59 +0530

    Unit-1:(6)

---
+++ College/six.py
@@ -0,0 +1,12 @@
+'''
+(6)=> Write a program to evulates an expressing given by the user at run
+time using eval() function.
+for example:
+Enter and expression -> 10+8-9*2-(10*2)
+Result -> -20
+'''
+
+a=input("Enter Expression to evulate->")
+
+print("Result=>",eval(a))
+
