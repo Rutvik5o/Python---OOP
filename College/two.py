85010a2e1988d1848ab74b5fa564d6b906abc4b1
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-28 12:48:47 +0530

    Unit-1:(2)

---
+++ College/two.py
@@ -0,0 +1,9 @@
+'''
+(2) => Write a prgram to display sum of two numbers.
+'''
+
+num1 = int(input("Enter first number->"))
+
+num2 = int(input("Enter second number->"))
+
+print("Addition=> ",(num1+num2))
