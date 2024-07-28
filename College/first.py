38a1522a97100e1e97b4da665e766112ca416907
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-28 12:47:50 +0530

    Add files via upload

---
+++ College/first.py
@@ -0,0 +1,14 @@
+'''
+(1)=> Write a program to swap two numbers without taking a temporary variable.
+
+'''
+
+a=int(input("Enter the value of a-> "))
+
+b=int(input("Enter the value of b-> "))
+
+a,b=b,a
+
+print("Value of a-> ",a)
+
+print("Value of b-> ",b)
