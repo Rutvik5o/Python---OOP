e0a63e2661df87bd17f527388175d86bacd00e40
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-07-31 20:11:43 +0530

    Unit-2:(2)

---
+++ College/12.py
@@ -0,0 +1,27 @@
+'''
+(2)=> Create a program to retrieve,display and update only a range
+of elemnets from an array using indexing and slicing in arrays
+'''
+
+
+a=[0,1,2,4,6,8]
+
+b=a[0:2]
+
+
+print("printing full array->")
+print(a)
+
+print("print array using index->")
+print(a[2])
+
+print("print array using slicing")
+
+b=a[0:2]
+print(b)
+
+c=a[:3]
+print(c)
+
+d=a[0:4]
+print(d)
