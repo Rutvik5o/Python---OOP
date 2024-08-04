9e6558b1cd2720c046e04a9d437f18eb3f96601a
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-08-04 21:44:07 +0530

    Unit-2:(8)

---
+++ College/18.py
@@ -0,0 +1,11 @@
+'''
+(8)=> Write a program to pass a list to function and display it.
+'''
+
+
+def my_function(food):
+    for x in food:
+        print(x)
+
+print("Printing list:")
+my_function(['apple', 'watermelon', 'orange'])
