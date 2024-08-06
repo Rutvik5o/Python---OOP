3a5590d390ff2b0ac7e9bcb06fe198ef59d57920
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-08-06 21:55:05 +0530

    Unit-2:(23)

---
+++ College/33.py
@@ -0,0 +1,19 @@
+'''
+(23)=> Create a python function to accpect python function as a dictionary 
+and display its elements.
+'''
+
+
+
+
+def func(d):
+
+    for key in d:
+
+        print("Key",key,"Value",d[key])
+
+
+
+d = {'a':1,'b':2,'c':3}
+
+print(d)
