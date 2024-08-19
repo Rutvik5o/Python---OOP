3b22b36d6d10a5c49d7ac897776ff9b88b43eca6
Rutvik Prajapati <96343256+Rutvik5o@users.noreply.github.com>
2024-08-19 19:50:58 +0530

    Unit-2:(9)

--- College/19.py
+++ College/19.py
@@ -9,12 +9,13 @@ def my_function(fname,lname):
     print(fname + " "+lname)
 
 my_function("abc","xyz")
+#Positional arguments are passed in the order defined in the function.
 
 
 def my_function(child3,child2,child1):
 
     print("The youngest children->",child3)
-
+#Keyword arguments specify the parameter name and can be passed in any order.
 
 my_function(child1 = "one",child2= "second",child3="three")
 
@@ -22,6 +23,9 @@ my_function(child1 = "one",child2= "second",child3="three")
 def my_function(country="Norway"):
     
     print("i am from",country)
+
+#Default arguments are used if no value is provided for the parameter.
+
 
 my_function("India")
 my_function()
